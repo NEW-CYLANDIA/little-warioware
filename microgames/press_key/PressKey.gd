551e5505bd6bf217e9620309a6fb532226b883c5
Paul Joannon <hello@pauljoannon.com>
2023-03-25 23:27:09 +0100

    Lint

--- microgames/press_key/PressKey.gd
+++ microgames/press_key/PressKey.gd
@@ -1,16 +1,15 @@
 extends Microgame
 # A simple example microgame - press the key to win!
-# ...or maybe not - it gets tricky on harder levels! 
+# ...or maybe not - it gets tricky on harder levels!
 
+onready var press_sprite: AnimatedSprite = $Instructions/AnimatedSprite
 
-onready var press_sprite : AnimatedSprite = $Instructions/AnimatedSprite
-
-var velocity : Vector2
+var velocity: Vector2
 
 
 func _ready():
 	randomize()
-	velocity = Vector2(rand_range(-1,1), rand_range(-1,1)) * 500
+	velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)) * 500
 
 	# if difficulty medium or hard, chance to flip win condition
 	if session.level > Global.difficulty.EASY and randf() > 0.4:
@@ -37,4 +36,4 @@ func _input(event):
 		else:
 			is_success = true
 			press_sprite.animation = "win"
-			$WinSFX.play()			
+			$WinSFX.play()
