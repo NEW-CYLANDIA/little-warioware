581c1904e778d048caf3072409ed8a98a4d04147
wren <102561459+slimewitch@users.noreply.github.com>
2023-09-18 19:27:29 +0100

    Updated hotdog game to work with timer bool

--- microgames/hotdog_game/src/hot_dog.gd
+++ microgames/hotdog_game/src/hot_dog.gd
@@ -2,16 +2,14 @@ extends AnimatedSprite
 
 
 var FALL_SPEED = 16
-var falling = false
+var is_falling = false
 
 func _ready():
 	position.x = rand_range(200, 700)
 
 func _physics_process(_delta):
-	if falling == true:
+	if is_falling == true:
 		position.y += FALL_SPEED
-	else:
-		$falling.stop()
 
 
 
--- microgames/hotdog_game/src/hotdog_game.gd
+++ microgames/hotdog_game/src/hotdog_game.gd
@@ -3,31 +3,32 @@ extends Microgame
 var hotdog_collected := false
 
 func _ready():
-	$hotdog.falling = true
-	$hotdog/falling.play()
-	if is_difficulty_medium():
+	if is_difficulty_hard():
 		$hotdog.FALL_SPEED = 20
 
 func _physics_process(_delta):
+	if is_timer_running:
+		$hotdog.is_falling = true
 	if hotdog_collected:
 		#hotdog moves with hand
 		$hotdog.global_position = $hand/hotdog_marker.global_position
 
 func _on_bun_collision_area_entered(area):
 	if area.name == "hotdog_collision":
-		$hotdog.falling = false
+		$hotdog.is_falling = false
+		$hotdog/falling.stop()
 		hotdog_collected = true
-		if not is_difficulty_medium():
+		if not is_difficulty_easy():
 			#if medium or hard, ketchup falls
 			$ketchup.falling = true
 			$ketchup/falling.play()
-		else:
+		elif is_difficulty_easy():
 		#if difficulty is easy, hotdog wins and ketchup and mustard dont fall
 			is_success = true
 	if area.name == "ketchup_collision":
 		$ketchup.queue_free()
 		$hotdog.play("ketchup")
-		if is_difficulty_medium():
+		if is_difficulty_hard():
 			$mustard.falling = true
 			$mustard/falling.play()
 		else:
